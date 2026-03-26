408e5c2d236e48dbfa5e9feee6880d86cfb6f340
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-03-26 16:33:16 -0600

    Using @Slf4j to be able to use log.debug() in order to print messages, because using sout ain't a good practice

--- src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -1,12 +1,22 @@
 package guru.springframework.spring7restmvc.controller;
 
+import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    Beer getBeerById(UUID id) {
+        log.debug("Get Beer Id - in Controller");
+        return beerService.getBeerById(id);
+    }
 
 }
