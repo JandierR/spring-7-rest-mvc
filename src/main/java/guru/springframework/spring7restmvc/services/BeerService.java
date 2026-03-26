dd772ba92868dfb1b866aa99ae6e410177663b89
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-03-26 16:16:03 -0600

    Using @AllArgsConstructor for BeerController in order to create a constructor.

--- src/main/java/guru/springframework/spring7restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring7restmvc/services/BeerService.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring7restmvc.services;
 
 import guru.springframework.spring7restmvc.model.Beer;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface BeerService {
     Beer getBeerById(UUID id);
 }
