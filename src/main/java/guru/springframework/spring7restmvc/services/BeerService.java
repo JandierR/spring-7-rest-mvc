9d9688245e28e95034f4518415988a6844960749
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-03-27 09:32:55 -0600

    Using @RestController and @RequestMapping in order to apply the HTTP concept with GET and see how it works by obtaining the URL.

--- src/main/java/guru/springframework/spring7restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring7restmvc/services/BeerService.java
@@ -3,9 +3,12 @@ package guru.springframework.spring7restmvc.services;
 import guru.springframework.spring7restmvc.model.Beer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public interface BeerService {
+    List<Beer> beerList();
+
     Beer getBeerById(UUID id);
 }
