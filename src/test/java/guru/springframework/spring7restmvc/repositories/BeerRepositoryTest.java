ebfeb0eccaecbc1964794fb1980061be35ddda2b
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-09 11:02:36 -0600

    Adding the Beer and Customer repositories along with their Tests

---
+++ src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring7restmvc.repositories;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer() {
+        Beer beer = beerRepository.save(Beer.builder()
+                .beerName("New Name")
+                .build());
+
+        assertThat(beer.getId()).isNotNull();
+    }
+}
