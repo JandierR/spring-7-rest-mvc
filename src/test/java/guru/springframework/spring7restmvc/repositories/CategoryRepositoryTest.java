776d69a056bb4f9ea67bd51f4c55747dfbd17c50
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-21 15:15:58 -0600

    Adding CategoryRepository and creating its test to check category is added to a beer object.

---
+++ src/test/java/guru/springframework/spring7restmvc/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring7restmvc.repositories;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import guru.springframework.spring7restmvc.entities.Category;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testBeer = beerRepository.findAll().getFirst();
+    }
+
+    @Transactional
+    @Test
+    void testAddCategory() {
+        Category savedCat = categoryRepository.save(Category.builder()
+                .description("Ales")
+                .build());
+
+        testBeer.addCategory(savedCat);
+        Beer saveBeer = beerRepository.save(testBeer);
+
+        System.out.println(saveBeer.getBeerName());
+    }
+}
