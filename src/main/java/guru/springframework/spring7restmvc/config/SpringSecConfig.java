418c54600f76b7d5119db904103affd5d24bb4bf
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-27 10:13:35 -0600

    Some changes to light up OAuth2 Resource server

--- src/main/java/guru/springframework/spring7restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring7restmvc/config/SpringSecConfig.java
@@ -10,13 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
 
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
