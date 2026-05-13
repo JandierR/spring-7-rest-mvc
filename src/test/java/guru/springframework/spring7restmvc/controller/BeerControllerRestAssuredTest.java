8edc7b367077a153f135e06f81e4541fd196692e
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-13 15:23:21 -0600

    Some changes

--- src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerRestAssuredTest.java
@@ -7,9 +7,30 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring7restmvc")
 public class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestConfig{
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests(auth ->
+                    auth.anyRequest().permitAll());
+
+            return http.build();
+        }
+    }
 
     @LocalServerPort
     Integer localPort;
