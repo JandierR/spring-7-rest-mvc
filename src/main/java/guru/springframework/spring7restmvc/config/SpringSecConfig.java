263b85d21dcd050570b6ba2bcfd367b2bfe456a0
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-15 11:28:40 -0600

    Adding actuator and its configuration

--- src/main/java/guru/springframework/spring7restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring7restmvc/config/SpringSecConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring7restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFiltherChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint()).authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers(EndpointRequest.toAnyEndpoint())
+                        .permitAll()
+        );  
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authorize ->
